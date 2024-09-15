eb0aa76c045c4e82891aaf1f037002fb8253564e
Pozzi2002 <nik.stroganov.2019@mail.ru>
2024-09-15 13:47:23 +0300

    My finished project

--- js.js
+++ js.js
@@ -18,7 +18,7 @@ function addBookToLibrary() {
     const author = document.querySelector("#author").value;
     const title = document.querySelector("#title").value;
     const numberPages = document.querySelector("#numberPages").value;
-    const read = document.querySelector("#read").checked;
+    const read = document.querySelector("#read");
     myLibrary.push(new Book(author, title, numberPages, read))
 }
 //Dialog close and btn Listener
@@ -45,6 +45,7 @@ function loop() {
     const checkbox = document.createElement("input")
     checkbox.setAttribute("type", "checkbox")
     checkbox.checked = document.querySelector("#read").checked
+    console.log(checkbox.checked)
     const del = document.createElement("button")
     del.textContent = "Delete Book"
     del.className = "del"
@@ -52,10 +53,8 @@ function loop() {
     container.appendChild(oneBook)
 
     for(let book in myLibrary) {
-        oneBook.innerHTML = "<h3>Author:</h3>" + `<p>${myLibrary[book].author}</p>` +"<h3>Title:</h3>"+ `<p>${myLibrary[book].title}</p>` +"<h3>Number of pages:</h3>"+ `<p>${myLibrary[book].numberPages}</p>` + "<span class='sex'><h3 style='display:inline'>Read/Unread:</h3>"
-        const span = document.querySelector(".sex")
-        span.appendChild(checkbox)
-        oneBook.innerHTML += "</span>"
+        oneBook.innerHTML = "<h3>Author:</h3>" + `<p>${myLibrary[book].author}</p>` +"<h3>Title:</h3>"+ `<p>${myLibrary[book].title}</p>` +"<h3>Number of pages:</h3>"+ `<p>${myLibrary[book].numberPages}</p>` + "<h3 style='display:inline'>Read/Unread:</h3>"
+        oneBook.appendChild(checkbox)
         oneBook.appendChild(del)
     }
 //Button for delete book
